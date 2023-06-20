d714d546ae2e82eccdef396b5c7dc3a7772f038d
Rosario Burs <rosarioagostinaburs@gmail.com>
2023-06-20 17:45:33 -0300

    Modularizacion

--- prentrega/inicio.py
+++ prentrega/inicio.py
@@ -1,34 +1,13 @@
-import json
 import pathlib
-
+from paquete.manejo_de_datos import cargar_datos, mostrar_datos
+from paquete.usuarios import ingresar_datos
 
 BASE_DIR = pathlib.Path(__file__).resolve().parent
 RUTA_BASE_DATOS = BASE_DIR / "base_de_datos.json"
-base_datos = []
-
-
-def ingresar_datos():
-    usuarios = {}
-    usuarios["nombre"] = input("Nombre: ")
-    usuarios["password"] = input("Contraseña: ")
-    base_datos.append(usuarios)
-    guardar_datos()
-
-def guardar_datos():
-    with open(RUTA_BASE_DATOS, "w") as archivo:
-        json.dump(base_datos, archivo, indent=4)
-
-def cargar_datos():
-    with open(RUTA_BASE_DATOS, "r") as archivo:
-        global base_datos
-        base_datos = json.load(archivo)
-
-def mostrar_datos():
-    print(base_datos)
 
 def main():
-    cargar_datos()
-    ingresar_datos()
-    mostrar_datos()
-    
+    base_datos: list = cargar_datos(RUTA_BASE_DATOS)
+    ingresar_datos(RUTA_BASE_DATOS, base_datos)
+    mostrar_datos(base_datos)
+   
 main()
---
+++ prentrega/paquete/manejo_de_datos.py
@@ -0,0 +1,19 @@
+import json
+
+def guardar_datos(RUTA_BASE_DATOS, base_datos):
+    with open(RUTA_BASE_DATOS, "w") as archivo:
+        json.dump(base_datos, archivo, indent=4)
+
+def cargar_datos(RUTA_BASE_DATOS) -> list:
+    base_datos = []
+    try:
+        with open(RUTA_BASE_DATOS, "r") as archivo:
+            base_datos = json.load(archivo)
+    except json.decoder.JSONDecodeError:
+        print("El archivo existe pero esta corrupto.")
+    except FileExistsError:
+        print("La base de datos no existe, no se cargan datos")
+    return base_datos
+
+def mostrar_datos(base_datos):
+    print(base_datos)
